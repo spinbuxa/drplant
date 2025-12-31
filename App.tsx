6099f7fc35a2e37aabebc5cb2e4d02646e930aaa
spinbuxa <140452880+spinbuxa@users.noreply.github.com>
2025-12-31 10:14:54 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Navbar } from './components/Navbar';
+import { AppNavbar } from './components/AppNavbar';
 import { BottomNav } from './components/BottomNav';
 import { PlantWeather } from './components/PlantWeather';
 import { ImageUpload } from './components/ImageUpload';
@@ -346,7 +346,7 @@ const App: React.FC = () => {
 
   return (
     <div className="min-h-screen flex flex-col bg-slate-50 dark:bg-slate-900 transition-colors duration-300 pb-24">
-      <Navbar onReset={handleReset} isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
+      <AppNavbar onReset={handleReset} isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
       
       <main className="flex-grow container mx-auto px-4 py-4 max-w-2xl">
         {renderContent()}
